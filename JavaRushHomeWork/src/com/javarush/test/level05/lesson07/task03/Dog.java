343f38e5922d5423f8d995e43ece667036c8316b
Yurii Bespalko <stormsdontlast4ever@gmail.com>
2015-09-27 01:53:21 +0300

    resolved level05/lesson07/task03 task04

--- JavaRushHomeWork/src/com/javarush/test/level05/lesson07/task03/Dog.java
+++ JavaRushHomeWork/src/com/javarush/test/level05/lesson07/task03/Dog.java
@@ -10,5 +10,47 @@ package com.javarush.test.level05.lesson07.task03;
 public class Dog
 {
     //напишите тут ваш код
+    private String name;
+    private int height;
+    private String color;
+
+    public void initialize (String name) { this.name = name;
+    }
+
+    public void initialize (String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public void initialize (String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+//    Dog (String name) {
+//        this.name = name;
+//    }
+//
+//    Dog (String name, int height) {
+//        this.name = name;
+//        this.height = height;
+//    }
+//
+//    Dog (String name, int height, String color) {
+//        this.name = name;
+//        this.height = height;
+//        this.color = color;
+//    }
+
+//    public static void main (String... args) {
+//        Dog dog = new Dog("Bulldog");
+//        Dog dog1 = new Dog("TerTeryer", 7);
+//        Dog dog2 = new Dog("Flabber", 9, "Brown");
+//
+//        System.out.println(dog.name);
+//        System.out.println(dog1.name + " " + dog1.height);
+//        System.out.println(dog2.name + " " + dog2.height + " " + dog2.color);
+//    }
 
 }
--- JavaRushHomeWork/src/com/javarush/test/level05/lesson07/task04/Circle.java
+++ JavaRushHomeWork/src/com/javarush/test/level05/lesson07/task04/Circle.java
@@ -10,5 +10,31 @@ package com.javarush.test.level05.lesson07.task04;
 public class Circle
 {
     //напишите тут ваш код
+    private double centerX;
+    private double centerY;
+    private double radius;
+    private double width;
+    private String color;
+
+    public void initialize (double centerX, double centerY, double radius) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+    }
+
+    public void initialize (double centerX, double centerY, double radius, double width){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+        this.width = width;
+    }
+
+    public void initialize (double centerX, double centerY, double radius, double width, String color) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+        this.width = width;
+        this.color = color;
+    }
 
 }
